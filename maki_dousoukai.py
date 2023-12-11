3b806a6352140d2a1c05e0f797f4b5f427244d52
chaahan <stronepic@gamil.com>
2023-12-12 00:34:03 +0900

    first commit

---
+++ maki_dousoukai.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 10 00:15:29 2023
+
+@author: 81801
+"""
+
+import streamlit as st
+import pandas as pd
+
+
+
+st.write("<h1>水戸桜ノ牧高等学校の<br>プチ同窓会案内</h1>", unsafe_allow_html=True)
+st.caption('本同窓会は個人的に主催する者でオフィシャルなものではありません。')
+
+st.write("現在、参加予定の方は以下です。")
+
+df = pd.read_csv('meibo.csv',index_col='No.')
+st.dataframe(df)
+
+
+st.write("参加をする場合は以下二つの方法があります。")
+st.write("　　１．幹事の飯田、もしくは安達に何でもいいので連絡をする")
+st.write("　　　（代表して一人の人が、「〇〇と××と△△が参加します」みたいなのでもOK）")
+st.write("　 ２．以下入力フォームより")
