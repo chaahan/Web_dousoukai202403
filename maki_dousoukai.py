0fd959886a0a0f0cd4aa7f04cce8379234c06a98
chaahan <stronepic@gamil.com>
2023-12-15 16:26:50 +0900

    入力欄追加

--- maki_dousoukai.py
+++ maki_dousoukai.py
@@ -19,7 +19,11 @@ df = pd.read_csv('meibo.csv',index_col='No.')
 st.dataframe(df)
 
 
-st.write("参加をする場合は以下二つの方法があります。")
+st.write("参加をする場合は以下二つの方法で")
 st.write("　　１．幹事の飯田、もしくは安達に何でもいいので連絡をする")
+st.write("　　   LINE,instgram何でもいいです")
 st.write("　　　（代表して一人の人が、「〇〇と××と△△が参加します」みたいなのでもOK）")
-st.write("　 ２．以下入力フォームより")
+st.write("　 ２．以下入力フォームより入力")
+name = st.text_input("氏名")
+plan = st.selectbox("参加可否",["参加","シフト次第で参加","参加したけど未定"])
+
